573304bbdef0d090f39a5b51f7ad1f26473cfb32
Anthony Gamon <anthonygamon@Anthonys-MBP-2.attlocal.net>
2020-11-28 14:39:38 -0600

    working

---
+++ script.js
@@ -0,0 +1,8 @@
+var a2a_config = a2a_config || {};
+a2a_config.overlays = a2a_config.overlays || [];
+a2a_config.overlays.push({
+    services: ['sms', 'facebook', 'tumblr', 'copy_link'],
+    size: '35',
+    style: 'horizontal',
+    position: 'top left'
+});
